1c88273b7b0d7ab480ac9db5886f9c630772b0d4
Eyosias Desta <mamesmsdesta@gmail.com>
2022-05-17 18:06:22 +0300

    image configuration done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
+import Image from "next/image"
 import Typography from "@material-ui/core/Typography"
 import Button from '@material-ui/core/Button'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
@@ -11,13 +12,26 @@ const useStyles = makeStyles(() =>
     },
   })
 );
-const Home: NextPage = () => {
+const Home: NextPage = ({carLists}: any) => {
   const classes = useStyles();
   return (
     <>
-      <Typography variant="h6">to be done</Typography>
+    <Image src={carLists[2].imageUrl} alt="car image" width="200" height="200"/>
+      <Typography variant="h6">to be done {carLists[2].id}</Typography>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await fetch("https://api.staging.myautochek.com/v1/inventory/make?popular=true");
+  const {makeList} = await res.json();
+
+
+  return {
+    props: {
+      carLists: makeList
+    }
+  }
 }
 
 export default Home;
